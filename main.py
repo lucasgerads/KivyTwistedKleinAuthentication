b5e2fe589917f62b3672bf37b47dfec5cb26cab3
Lucas Gerads <lucas.gerads@gmail.com>
2023-02-06 07:14:28 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# install_twisted_rector must be called before importing and using the reactor
+from kivy.support import install_twisted_reactor
+install_twisted_reactor()
+
+from webserver import webapp
+from twisted.web.server import Site
+from twisted.internet import reactor, endpoints
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+class TwistedServerApp(App):
+    label = None
+
+    def build(self):
+        self.label = Label(text="server started\n")
+        endpoint = endpoints.serverFromString(reactor, "tcp:8080")
+        endpoint.listen(Site(webapp.resource()))
+        return self.label
+
+    def handle_message(self, msg):
+        self.label.text = "requests handled:  {}\n".format(msg)
+        return "some response" 
+
+if __name__ == '__main__':
+    TwistedServerApp().run()
---
+++ webserver.py
@@ -0,0 +1,67 @@
+from klein import Klein
+from twisted.web.static import File
+from twisted.web.util import redirectTo
+from twisted.internet.defer import succeed
+import random
+import json
+from hashlib import pbkdf2_hmac
+
+webapp = Klein()
+
+requestCounter = 0
+
+from kivy.app import App
+
+@webapp.route('/')
+def root(request):
+    return redirectTo(b"index", request)
+
+@webapp.route('/index')
+def index(request):
+    global requestCounter 
+    requestCounter += 1
+    app = App.get_running_app()
+    app.handle_message(requestCounter)
+    return File("index.html")
+
+@webapp.route('/assets/<filename>')
+def assets(request, filename):
+    path = "assets/" + filename
+    return File(path) 
+
+@webapp.route('/login')
+def login(request):
+    return File("login.html")
+
+@webapp.route('/success')
+def success(request):
+    return File("success.html")
+
+@webapp.route('/fail')
+def fail(request):
+    return File("fail.html")
+
+@webapp.route('/submit', methods=['POST'])
+def submit(request):
+    requestResponse = request.content.read()
+    password = requestResponse.decode("utf-8").split('=')
+    userPassword = pbkdf2_hmac('sha256', password[1].encode('utf-8'), b'some salt'*2, 1000)
+    storedPassword =  pbkdf2_hmac('sha256', b'1234', b'some salt'*2, 1000)
+    if (userPassword == storedPassword ):
+        request.redirect('/success')
+        return succeed(None)
+    else:
+        request.redirect('/fail')
+        return succeed(None)
+
+@webapp.route('/api/random')
+def randomData(request):
+    # how do i make this a protected route
+    data = {}
+    someData = []
+    for i in range(10):
+        someData.append(random.randrange(start = 0, stop = 100, step=1))
+    data['apidata'] = someData
+    json_data = json.dumps(data)
+    print(json_data)
+    return json_data 
